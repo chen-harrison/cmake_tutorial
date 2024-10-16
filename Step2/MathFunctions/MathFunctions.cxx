4d2713031644e1e69fd269024c913f1f884f3cb9
Harrison Chen <hchen.robotics@gmail.com>
2024-10-16 10:28:03 -0400

    Step 2, TODO 11

--- Step2/MathFunctions/MathFunctions.cxx
+++ Step2/MathFunctions/MathFunctions.cxx
@@ -1,15 +1,25 @@
 #include "MathFunctions.h"
 
 // TODO 11: include cmath
+#include <cmath>
+#include <iostream>
 
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
-#include "mysqrt.h"
+#ifdef USE_MYMATH
+  #include "mysqrt.h"
+#endif
 
 namespace mathfunctions {
 double sqrt(double x)
 {
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
-  return detail::mysqrt(x);
+  #ifdef USE_MYMATH
+    std::cout << "using detail::mysqrt()\n";
+    return detail::mysqrt(x);
+  #else
+    std::cout << "using std::sqrt()\n";
+    return std::sqrt(x);
+  #endif
 }
 }
