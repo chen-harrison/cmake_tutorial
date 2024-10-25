4fc20d6237f2f416f79a1a1063b92bf9638ee6bf
Harrison Chen <hchen.robotics@gmail.com>
2024-10-25 10:17:10 -0400

    Step 2, TODO 14

--- Step2/MathFunctions/MathFunctions.cxx
+++ Step2/MathFunctions/MathFunctions.cxx
@@ -15,10 +15,8 @@ double sqrt(double x)
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
   #ifdef USE_MYMATH
-    std::cout << "using detail::mysqrt()\n";
     return detail::mysqrt(x);
   #else
-    std::cout << "using std::sqrt()\n";
     return std::sqrt(x);
   #endif
 }
